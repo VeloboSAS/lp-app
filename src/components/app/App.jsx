5d183296cfdd30f353033f59eb6c5d5455e752ad
Alex <alexss81@mail.ru>
2024-04-13 22:23:03 +0300

    creating a project structure

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,11 +1,15 @@
+import Footer from "../Footer/Footer"
+import Header from "../Header/Header"
+import AppRoutes from "./AppRoutes"
 
-function App() {
+const App = () => (
 
-  return (
-    <>
-      <div>App</div>
-    </>
+      <div className="app">
+        <Header/>
+        <AppRoutes/>
+        <Footer/>
+      </div>
+
   )
-}
 
 export default App
